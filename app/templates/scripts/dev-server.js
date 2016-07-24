442d4bee5a575e0ac1c380167458f514bc4799a8
Matt McFarland <contact@mattmcfarland.com>
2016-07-24 13:24:00 -0400

    fix lint issues with dev-server.js

--- app/templates/scripts/dev-server.js
+++ app/templates/scripts/dev-server.js
@@ -1,9 +1,7 @@
 #!/usr/bin/env node
+
 var budo = require('budo')
-var path = require('path')
-var es2040 = require('es2040')
-var sheetify = require('sheetify/transform')
-var inspect = require('util').inspect;
+var inspect = require('util').inspect
 
 budo('./src/index.js', {
   browserifyArgs: [
@@ -11,13 +9,12 @@ budo('./src/index.js', {
     '-g', 'es2040'
   ],
   debug: true,
-  //dir: path.join(__dirname, '../', 'static'),
   host: 'localhost',
   live: true,
   open: true,
   port: 3000,
   pushState: true,
-  stream: process.stdout,
-}).on('connect', function(ev) {
+  stream: process.stdout
+}).on('connect', (ev) => {
   console.log(inspect(ev, {depth: 10, colors: true}))
 })
