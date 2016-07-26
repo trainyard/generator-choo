de9a2aa1a0ff513d36d8517d53f2e96a1f78f20e
YerkoPalma <yerko.palma@usach.cl>
2016-07-25 22:36:22 -0400

    template: add choo-log in dev environment

--- app/templates/src/index.js
+++ app/templates/src/index.js
@@ -5,6 +5,11 @@ sf('normalize.css', { global: true })
 sf('./styles/main.css', { global: true })
 
 const app = choo()
+if (process.env.NODE_ENV !== 'production') {
+  const log = require('choo-log')
+  app.use(log())
+}
+
 app.model(require('./models/todos'))
 app.router(require('./routes'))
 
